4b5b09bb1613390e88db7a84b424826c15aa47d7
Onkar Kumar <onkarmishra.dev@gmail.com>
2024-04-24 12:01:14 +0530

    made button and label to call the api

---
+++ AppToCallPackage/Controller/CallPackageVC.swift
@@ -0,0 +1,32 @@
+//
+//  CallPackageVC.swift
+//  AppToCallPackage
+//
+//  Created by Onkar Mishra on 24/04/24.
+//
+
+import UIKit
+
+class CallPackageVC: UIViewController {
+
+    @IBOutlet var responseLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func btnActionToCallPackage(_ sender: Any) {
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
