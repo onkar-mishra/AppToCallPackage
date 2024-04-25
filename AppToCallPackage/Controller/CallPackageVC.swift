149a9d27184c3fa17ef205b122c13b04df25641d
Onkar Kumar <onkarmishra.dev@gmail.com>
2024-04-25 18:59:34 +0530

    Refactor CallPackageVC class
    - Split functionality into smaller, focused functions for better readability and maintainability.
    - Updated comments for clarity and adherence to best practices.

--- AppToCallPackage/Controller/CallPackageVC.swift
+++ AppToCallPackage/Controller/CallPackageVC.swift
@@ -6,27 +6,61 @@
 //
 
 import UIKit
+import SwiftUI
+import MyListPackage
 
 class CallPackageVC: UIViewController {
 
     @IBOutlet var responseLabel: UILabel!
+    
+    private var hostingController: UIHostingController<ListView>?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
+
     @IBAction func btnActionToCallPackage(_ sender: Any) {
+        presentListView()
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func presentListView() {
+        let listView = ListView(didSelectEmailAction: { [weak self] selectedEmail in
+            self?.updateResponseLabel(selectedEmail)
+            self?.dismissListView()
+        })
+        
+        presentListViewWithController(listView)
     }
-    */
 
+    private func presentListViewWithController(_ listView: ListView) {
+        hostingController = UIHostingController(rootView: listView)
+        
+        guard let hostingController = hostingController else { return }
+        
+        addHostingControllerToView(hostingController)
+    }
+
+    private func addHostingControllerToView(_ hostingController: UIHostingController<ListView>) {
+        hostingController.view.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(hostingController.view)
+        
+        NSLayoutConstraint.activate([
+            hostingController.view.topAnchor.constraint(equalTo: view.topAnchor),
+            hostingController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            hostingController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            hostingController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+        
+        addChild(hostingController)
+    }
+
+    private func updateResponseLabel(_ selectedEmail: String) {
+        responseLabel.text = selectedEmail
+    }
+
+    private func dismissListView() {
+        hostingController?.willMove(toParent: nil)
+        hostingController?.view.removeFromSuperview()
+        hostingController?.removeFromParent()
+    }
 }
